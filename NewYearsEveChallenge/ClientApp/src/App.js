7610dd707b59adaa417b6050be153aeca8140be6
SzymonNowak <nnja5@o2.pl>
2020-12-31 20:02:44 +0100

    base of

--- NewYearsEveChallenge/ClientApp/src/App.js
+++ NewYearsEveChallenge/ClientApp/src/App.js
@@ -1,22 +1,7 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import React, { Component } from "react";
 
-import './custom.css'
+const App = () => {
+  return <>elo</>;
+};
 
-export default class App extends Component {
-  static displayName = App.name;
-
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
-}
+export default App;
--- NewYearsEveChallenge/ClientApp/src/index.js
+++ NewYearsEveChallenge/ClientApp/src/index.js
@@ -1,18 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
-
-ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
-  rootElement);
-
-registerServiceWorker();
-
+import App from "./App";
+ReactDOM.render(<App />, document.getElementById("root"));
