341d56b8b72e5e84a18e0b4b430f8e56e0062a4a
SzymonNowak <nnja5@o2.pl>
2020-12-31 20:52:50 +0100

    first square

--- NewYearsEveChallenge/ClientApp/src/App.js
+++ NewYearsEveChallenge/ClientApp/src/App.js
@@ -1,7 +1,17 @@
-import React, { Component } from "react";
+import React, { useRef } from "react";
+
+import "./main.css";
 
 const App = () => {
-  return <>elo</>;
+  const canvasRef = useRef(null);
+  const canvasObj = canvasRef.current;
+  const ctx = canvasObj.getContext("2d");
+  console.log(ctx);
+  return (
+    <>
+      <canvas class="wrapeer" id="tutorial"></canvas>
+    </>
+  );
 };
 
 export default App;
