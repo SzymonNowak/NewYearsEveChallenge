60408c0c379663fb8529b8d5ffa248f88f02c510
aggigie <aggigie46@gmail.com>
2021-01-01 16:35:02 +0100

    small clean up

--- QuickDraw-master/QuickDrawApp.py
+++ QuickDraw-master/QuickDrawApp.py
@@ -31,7 +31,7 @@ def main():
     pts = []
     iter = 0
     blackboard = np.zeros((420, 700, 3), dtype=np.uint8)
-    with open(f"data.csv") as f:
+    with open(f"watch2.csv") as f:
         lines = [line.rstrip('\n').replace("(", '').replace(")", '') for line in f]
         pts = [(int(cordStr.split(',')[0]), int(cordStr.split(',')[1])) for cordStr in lines]
     for i in range(1, len(pts)):
@@ -64,42 +64,6 @@ def main():
                 writer = csv.writer(sample_file, delimiter='\n', quoting=csv.QUOTE_MINIMAL)
                 writer.writerow(pts)
                 iter += 1
-                # print(pred_class, pred_probab)
-   #  emojis = get_QD_emojis()
-   # # print(names)
-   #  cap = cv2.VideoCapture(0)
-   #  Lower_green = np.array([110, 50, 50])
-   #  Upper_green = np.array([130, 255, 255])
-   #  pts = deque(maxlen=512)
-   #  blackboard = np.zeros((480, 640, 3), dtype=np.uint8)
-   #  digit = np.zeros((200, 200, 3), dtype=np.uint8)
-   #  pred_class = 0
-   #  iter = 0
-   #
-   #  while (cap.isOpened()):
-   #      ret, img = cap.read()
-   #      img = cv2.flip(img, 1)
-   #      hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-   #      kernel = np.ones((5, 5), np.uint8)
-   #      mask = cv2.inRange(hsv, Lower_green, Upper_green)
-   #      mask = cv2.erode(mask, kernel, iterations=2)
-   #      mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-   #      # mask=cv2.morphologyEx(mask,cv2.MORPH_CLOSE,kernel)
-   #      mask = cv2.dilate(mask, kernel, iterations=1)
-   #      res = cv2.bitwise_and(img, img, mask=mask)
-   #      cnts, heir = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2:]
-   #      center = None
-   #
-   #      if len(cnts) >= 1:
-   #          cnt = max(cnts, key=cv2.contourArea)
-   #          if cv2.contourArea(cnt) > 200:
-   #              ((x, y), radius) = cv2.minEnclosingCircle(cnt)
-   #              cv2.circle(img, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-   #              cv2.circle(img, center, 5, (0, 0, 255), -1)
-   #              M = cv2.moments(cnt)
-   #              center = (int(M['m10'] / M['m00']), int(M['m01'] / M['m00']))
-   #              pts.appendleft(center)
-   #              # print (pts)
 
 
 def keras_predict(model, image):
