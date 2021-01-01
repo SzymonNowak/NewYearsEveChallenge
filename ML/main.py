440b4298ddb2028be0f93552fa4a4369ebfee790
aggigie <aggigie46@gmail.com>
2021-01-01 16:27:51 +0100

    reading data.csv, prediction could work

--- ML/main.py
+++
@@ -1,8 +0,0 @@
-
-def data_in() -> str:
-    result = ''
-    return result
-
-
-if __name__ == "__main__":
-    data_in()
--- QuickDraw-master/QuickDrawApp.py
+++ QuickDraw-master/QuickDrawApp.py
@@ -28,78 +28,78 @@ names = [
 
 
 def main():
-    emojis = get_QD_emojis()
-   # print(names)
-    cap = cv2.VideoCapture(0)
-    Lower_green = np.array([110, 50, 50])
-    Upper_green = np.array([130, 255, 255])
-    pts = deque(maxlen=512)
-    blackboard = np.zeros((480, 640, 3), dtype=np.uint8)
-    digit = np.zeros((200, 200, 3), dtype=np.uint8)
-    pred_class = 0
+    pts = []
     iter = 0
+    blackboard = np.zeros((420, 700, 3), dtype=np.uint8)
+    with open(f"data.csv") as f:
+        lines = [line.rstrip('\n').replace("(", '').replace(")", '') for line in f]
+        pts = [(int(cordStr.split(',')[0]), int(cordStr.split(',')[1])) for cordStr in lines]
+    for i in range(1, len(pts)):
+        if pts[i - 1] is None or pts[i] is None:
+            continue
+        cv2.line(blackboard, pts[i - 1], pts[i], (255, 255, 255), 7)
+  #  while True:
+   #     cv2.imshow("Frame", blackboard)
+    #    k = cv2.waitKey(10)
 
-    while (cap.isOpened()):
-        ret, img = cap.read()
-        img = cv2.flip(img, 1)
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        kernel = np.ones((5, 5), np.uint8)
-        mask = cv2.inRange(hsv, Lower_green, Upper_green)
-        mask = cv2.erode(mask, kernel, iterations=2)
-        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-        # mask=cv2.morphologyEx(mask,cv2.MORPH_CLOSE,kernel)
-        mask = cv2.dilate(mask, kernel, iterations=1)
-        res = cv2.bitwise_and(img, img, mask=mask)
-        cnts, heir = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2:]
-        center = None
 
-        if len(cnts) >= 1:
-            cnt = max(cnts, key=cv2.contourArea)
-            if cv2.contourArea(cnt) > 200:
-                ((x, y), radius) = cv2.minEnclosingCircle(cnt)
-                cv2.circle(img, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-                cv2.circle(img, center, 5, (0, 0, 255), -1)
-                M = cv2.moments(cnt)
-                center = (int(M['m10'] / M['m00']), int(M['m01'] / M['m00']))
-                pts.appendleft(center)
-                # print (pts)
-                for i in range(1, len(pts)):
-                    if pts[i - 1] is None or pts[i] is None:
-                        continue
-                    cv2.line(blackboard, pts[i - 1], pts[i], (255, 255, 255), 7)
-                    cv2.line(img, pts[i - 1], pts[i], (0, 0, 255), 2)
-        elif len(cnts) == 0:
-            if len(pts) != []:
-                blackboard_gray = cv2.cvtColor(blackboard, cv2.COLOR_BGR2GRAY)
-                blur1 = cv2.medianBlur(blackboard_gray, 15)
-                blur1 = cv2.GaussianBlur(blur1, (5, 5), 0)
-                thresh1 = cv2.threshold(blur1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-                
-                # print(cv2.findContours(thresh1.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE))
-                blackboard_cnts = cv2.findContours(thresh1.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
-            
-                if len(blackboard_cnts) >= 1:
-                    cnt = max(blackboard_cnts, key=cv2.contourArea)
-                    #print(cv2.contourArea(cnt))
-                    if cv2.contourArea(cnt) > 2000:
-                        x, y, w, h = cv2.boundingRect(cnt)
-                        digit = blackboard_gray[y:y + h, x:x + w]
-                        pred_probab, pred_class = keras_predict(model, digit)
-                        print(pts)
-                        print(names[pred_class])
-                        with open(str(names[pred_class])+str(iter)+'.csv', mode='w') as sample_file:
-                            writer = csv.writer(sample_file, delimiter='\n', quoting=csv.QUOTE_MINIMAL)
-                            writer.writerow(pts)
-                            iter += 1
-                        #print(pred_class, pred_probab)
+    blackboard_gray = cv2.cvtColor(blackboard, cv2.COLOR_BGR2GRAY)
+    blur1 = cv2.medianBlur(blackboard_gray, 15)
+    blur1 = cv2.GaussianBlur(blur1, (5, 5), 0)
+    thresh1 = cv2.threshold(blur1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-            pts = deque(maxlen=512)
-            blackboard = np.zeros((480, 640, 3), dtype=np.uint8)
-            img = overlay(img, emojis[pred_class], 400, 250, 100, 100)
-        cv2.imshow("Frame", img)
-        k = cv2.waitKey(10)
-        if k == 27:
-            break
+    # print(cv2.findContours(thresh1.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE))
+    blackboard_cnts = cv2.findContours(thresh1.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
+
+    if len(blackboard_cnts) >= 1:
+        cnt = max(blackboard_cnts, key=cv2.contourArea)
+        # print(cv2.contourArea(cnt))
+        if cv2.contourArea(cnt) > 2000:
+            x, y, w, h = cv2.boundingRect(cnt)
+            digit = blackboard_gray[y:y + h, x:x + w]
+            pred_probab, pred_class = keras_predict(model, digit)
+            print(pts)
+            print(names[pred_class])
+            with open(str(names[pred_class]) + str(iter) + '.csv', mode='w') as sample_file:
+                writer = csv.writer(sample_file, delimiter='\n', quoting=csv.QUOTE_MINIMAL)
+                writer.writerow(pts)
+                iter += 1
+                # print(pred_class, pred_probab)
+   #  emojis = get_QD_emojis()
+   # # print(names)
+   #  cap = cv2.VideoCapture(0)
+   #  Lower_green = np.array([110, 50, 50])
+   #  Upper_green = np.array([130, 255, 255])
+   #  pts = deque(maxlen=512)
+   #  blackboard = np.zeros((480, 640, 3), dtype=np.uint8)
+   #  digit = np.zeros((200, 200, 3), dtype=np.uint8)
+   #  pred_class = 0
+   #  iter = 0
+   #
+   #  while (cap.isOpened()):
+   #      ret, img = cap.read()
+   #      img = cv2.flip(img, 1)
+   #      hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+   #      kernel = np.ones((5, 5), np.uint8)
+   #      mask = cv2.inRange(hsv, Lower_green, Upper_green)
+   #      mask = cv2.erode(mask, kernel, iterations=2)
+   #      mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+   #      # mask=cv2.morphologyEx(mask,cv2.MORPH_CLOSE,kernel)
+   #      mask = cv2.dilate(mask, kernel, iterations=1)
+   #      res = cv2.bitwise_and(img, img, mask=mask)
+   #      cnts, heir = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2:]
+   #      center = None
+   #
+   #      if len(cnts) >= 1:
+   #          cnt = max(cnts, key=cv2.contourArea)
+   #          if cv2.contourArea(cnt) > 200:
+   #              ((x, y), radius) = cv2.minEnclosingCircle(cnt)
+   #              cv2.circle(img, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+   #              cv2.circle(img, center, 5, (0, 0, 255), -1)
+   #              M = cv2.moments(cnt)
+   #              center = (int(M['m10'] / M['m00']), int(M['m01'] / M['m00']))
+   #              pts.appendleft(center)
+   #              # print (pts)
 
 
 def keras_predict(model, image):
